b04286b614e4ef4bc438f2f5e535a62f7bff5b15
DerekGn <email@example.com>
2025-02-26 23:13:29 +0000

    Updated monitoring service

--- src/Shaos/Hosting/MonitorBackgroundWorker.cs
+++ src/Shaos/Hosting/MonitorBackgroundWorker.cs
@@ -1,6 +1,45 @@
+
+
 namespace Shaos.Hosting
 {
-    public class MonitorBackgroundWorker : BackgroundService
+    public class MonitorBackgroundWorker : IHostedService
     {
-    }    
+        private readonly ILogger<MonitorBackgroundWorker> _logger;
+        
+        public MonitorBackgroundWorker(
+            ILogger<MonitorBackgroundWorker> logger,
+            IHostApplicationLifetime hostApplicationLifetime)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            
+            hostApplicationLifetime.ApplicationStarted.Register(OnStarted);
+            hostApplicationLifetime.ApplicationStopped.Register(OnStopped);
+            hostApplicationLifetime.ApplicationStopping.Register(OnStopping);
+        }
+
+        private void OnStopping()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnStopped()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnStarted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
