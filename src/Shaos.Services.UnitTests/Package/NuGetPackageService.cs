d217041e0c2d7cb8619d0ce99e8dc89d69fe6176
DerekGn <dg89327@hotmail.com>
2025-03-11 20:10:28 +0000

    Updated NuGetPackageService

--- src/Shaos.Services.UnitTests/Package/NuGetPackageService.cs
+++ src/Shaos.Services.UnitTests/Package/NuGetPackageService.cs
@@ -1,4 +1,28 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿/*
+* MIT License
+*
+* Copyright (c) 2025 Derek Goslin https://github.com/DerekGn
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Shaos.Services.Options;
@@ -11,35 +35,62 @@ namespace Shaos.Services.UnitTests.Package
     public class NuGetPackageServiceTests : BaseUnitTests
     {
         private readonly NuGetPackageService _nuGetPackageService;
-        private readonly IOptions<NuGetPackageServiceOptions>? _options;
         private readonly ITestOutputHelper _output;
 
         public NuGetPackageServiceTests(ITestOutputHelper output)
         {
             var factory = ServiceProvider.GetService<ILoggerFactory>();
-            _options = ServiceProvider.GetService<IOptions<NuGetPackageServiceOptions>>();
+            var optionsInstance = new NuGetPackageServiceOptions()
+            {
+                PackageFolder = Path.Combine(Environment.CurrentDirectory, "Packages"),
+                PackageSources = new List<Uri>() { new Uri("https://api.nuget.org/v3/index.json") }
+            };
+
+            IOptions<NuGetPackageServiceOptions> options = Microsoft.Extensions.Options.Options.Create(optionsInstance);
 
             _nuGetPackageService = new NuGetPackageService(
                 factory!.CreateLogger<NuGetPackageService>(),
-                _options);
+                options);
+            _output = output;
         }
 
-        [Fact(Skip = "Not resolving")]
-        public async Task TestResolvePackagesAsync()
+        [Fact]
+        public async Task TestResolvePackageFoundAsync()
         {
-            var packagesToResolve = new List<NuGetPackageResolveRequest>()
+            var packageToResolve = new NuGetPackageResolveRequest()
             {
-                new NuGetPackageResolveRequest()
-                {
-                    PackageName = "HexIO",
-                    PackageVersion = "5.0.1"
-                }
+                Package = "HexIO",
+                Version = "5.0.1"
             };
 
-            var resolved = await _nuGetPackageService.ResolvePackagesAsync(packagesToResolve);
+            var result = await _nuGetPackageService.ResolvePackagesAsync(packageToResolve);
 
-            Assert.NotNull(resolved);
-            Assert.Single(resolved);
+            Assert.NotNull(result);
+            Assert.NotNull(result.Identity);
+            Assert.NotNull(result.Request);
+            Assert.Equal(ResolveStatus.Success, result.Status);
+            Assert.NotNull(result.Dependencies);
+            Assert.Single(result.Dependencies);
+        }
+
+        [Fact]
+        public async Task TestResolvePackageNotFoundAsync()
+        {
+            var packageToResolve =
+                new NuGetPackageResolveRequest()
+                {
+                    Package = "287391",
+                    Version = "9.9.9"
+                };
+
+            var result = await _nuGetPackageService.ResolvePackagesAsync(packageToResolve);
+
+            Assert.NotNull(result);
+            Assert.Null(result.Identity);
+            Assert.NotNull(result.Request);
+            Assert.Equal(ResolveStatus.NotFound, result.Status);
+            Assert.NotNull(result.Dependencies);
+            Assert.Empty(result.Dependencies);
         }
     }
 }
