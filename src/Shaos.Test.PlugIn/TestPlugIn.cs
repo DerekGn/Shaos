78ebaa6a3ede940f58ee4b94ad78c2f2e4a54e80
DerekGn <dg89327@hotmail.com>
2025-03-20 20:53:27 +0000

    Updated Test PlugIn logging

--- src/Shaos.Test.PlugIn/TestPlugIn.cs
+++ src/Shaos.Test.PlugIn/TestPlugIn.cs
@@ -14,10 +14,15 @@ namespace Shaos.Test.PlugIn
 
         public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Starting [{Name}].[{Operation}]", nameof(TestPlugIn), nameof(ExecuteAsync));
+
             do
             {
                 await Task.Delay(100, cancellationToken);
+                _logger.LogInformation("Executing [{Name}].[{Operation}]", nameof(TestPlugIn), nameof(ExecuteAsync));
             } while (cancellationToken.IsCancellationRequested);
+
+            _logger.LogInformation("Completed [{Name}].[{Operation}]", nameof(TestPlugIn), nameof(ExecuteAsync));
         }
     }
 }
