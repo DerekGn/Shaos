3887e4cf500c2e2acc1ae7fb5c20bb9b555da3fb
DerekGn <dg89327@hotmail.com>
2025-03-20 13:43:04 +0000

    Updated the PlugIn project

--- src/Shaos.Test.PlugIn/TestPlugIn.cs
+++ src/Shaos.Test.PlugIn/TestPlugIn.cs
@@ -1,9 +1,17 @@
-﻿using Shaos.Sdk;
+﻿using Microsoft.Extensions.Logging;
+using Shaos.Sdk;
 
 namespace Shaos.Test.PlugIn
 {
     public class TestPlugIn : IPlugIn
     {
+        private readonly ILogger<TestPlugIn> _logger;
+
+        public TestPlugIn(ILogger<TestPlugIn> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
             do
