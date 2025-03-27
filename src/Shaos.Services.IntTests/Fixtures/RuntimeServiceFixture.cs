e0fb109d6926f67bdac782d569ff91ce71bfcf82
DerekGn <dg89327@hotmail.com>
2025-03-27 17:42:43 +0000

    Updated the integration test fixtures

--- src/Shaos.Services.IntTests/Fixtures/RuntimeServiceFixture.cs
+++ src/Shaos.Services.IntTests/Fixtures/RuntimeServiceFixture.cs
@@ -22,36 +22,17 @@
 * SOFTWARE.
 */
 
-using Shaos.Services.Runtime;
 using System.IO.Compression;
-using System.Reflection;
 
 namespace Shaos.Services.IntTests.Fixtures
 {
-    public class PlugInFactoryTestFixture : TestFixture
+    public class RuntimeServiceFixture : TestFixture
     {
-        public PlugInFactoryTestFixture()
+        public RuntimeServiceFixture()
         {
             var assemblyDirectory = Path.Combine(BinariesPath, "1");
 
             ZipFile.ExtractToDirectory(PackageFilePath, assemblyDirectory, true);
-
-            var assemblyFilePath = Path.Combine(assemblyDirectory, AssemblyFileName);
-
-            AssemblyName = new AssemblyName(Path.GetFileNameWithoutExtension(assemblyFilePath));
-            var assemblyLoadContext = new RuntimeAssemblyLoadContext(nameof(PlugInFactoryTestFixture), assemblyFilePath);
-
-            AssemblyLoadContextReference = new UnloadingWeakReference<RuntimeAssemblyLoadContext>(assemblyLoadContext);
-        }
-
-        public UnloadingWeakReference<RuntimeAssemblyLoadContext> AssemblyLoadContextReference { get; }
-        public AssemblyName AssemblyName { get; }
-
-        public override void Dispose()
-        {
-            AssemblyLoadContextReference.Dispose();
-
-            base.Dispose();
         }
     }
 }
