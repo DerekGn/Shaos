846997bf60def50057183b08a22dbdfafe379ffb
DerekGn <DerekGn@users.noreply.github.com>
2026-06-22 17:46:15 +0100

    Remove datetime Offset
    SQLite does not support datetimeoffset
    Remove datetime offset
    Added client side timezone function

--- src/Shaos.Repository/Models/BaseEntityTracked.cs
+++ src/Shaos.Repository/Models/BaseEntityTracked.cs
@@ -38,21 +38,9 @@ namespace Shaos.Repository.Models
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
 
         /// <summary>
-        /// The created date
-        /// </summary>
-        [DisplayFormat(DataFormatString = "{0:f}")]
-        public DateTimeOffset CreatedDateOffset => new(CreatedDate);
-
-        /// <summary>
         /// The updated date
         /// </summary>
         [DisplayFormat(DataFormatString = "{0:f}")]
         public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
-
-        /// <summary>
-        /// The updated date
-        /// </summary>
-        [DisplayFormat(DataFormatString = "{0:f}")]
-        public DateTimeOffset UpdatedDateOffset => new(CreatedDate);
     }
 }
