2e7afc236398e0a222328a77cdcc8dd3d98894e9
DerekGn <derekgn@users.noreply.github.com>
2026-07-07 22:36:01 +0100

    Updated log tracing

--- src/Shaos/wwwroot/js/log.js
+++ src/Shaos/wwwroot/js/log.js
@@ -1,15 +1,32 @@
 ﻿const eventSource = new EventSource('/api/v1/log-events');
 const trace = document.getElementById('trace');
+const maxTraceLines = 100;
 
 window.onload = function () {
     eventSource.addEventListener('log-created-event', (event) => {
         console.debug('log-created-event');
 
         if (trace != null) {
-            var div = document.createElement("div");
-            div.innerHTML = event.data;
+            const traceLines = trace.value.split('\n');
+            const logEvent = JSON.parse(event.data);
+            const newTraceLines = logEvent.log.split('\r\n');
 
-            trace.appendChild(div);
+            if (traceLines.length > maxTraceLines) {
+
+                var count = 0;
+
+                for (var i = 0; i < newTraceLines.length; i++) {
+                    count += traceLines[i].length + 1;
+                }
+
+                console.debug("Trace Lines: [" + traceLines.length + "] New Trace Lines: [" + newTraceLines.length + "] Count: [" + count + "]");
+
+                trace.value = trace.value.slice(count);
+            }
+
+            trace.value += logEvent.log;
+
+            trace.setSelectionRange(trace.value.length, trace.value.length);
         }
         else {
             console.warn("trace is null");
